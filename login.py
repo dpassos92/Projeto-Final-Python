f9c202584656c4a2526f4be364dd1f1a009ffaaf
dpassos92 <passos.diogo92@gmail.com>
2024-03-13 09:28:38 +0000

    Inicio do Projeto
    Co-Authored-By: TiagoRochaSousa <159248569+tiagorochasousa@users.noreply.github.com>
    Co-Authored-By: brunohlpinto <149703778+brunohlpinto@users.noreply.github.com>

---
+++ login.py
@@ -0,0 +1,11 @@
+from tkinter import*
+
+janela_login =Tk()
+janela_login.title("Login")
+
+mensagem = Label(janela_login, text='Bem-vindo ao GRANDE BAZZAR TUGA')
+mensagem.grid(column=0, row=0)
+usuario = StringVar()
+password=StringVar()
+
+janela_login.mainloop()
